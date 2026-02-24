336c68ee16ae1671ad9b51efd86945700390299d
SerginhoPizza <serginhos.monteiro@gmail.com>
2026-02-24 16:56:41 -0300

    Servidor API

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const VERIFY_TOKEN = "meu_token_123";
+
+// Rota GET (verificação da Meta)
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("Webhook verificado!");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+// Rota POST (receber mensagens)
+app.post("/webhook", (req, res) => {
+  console.log("Mensagem recebida:");
+  console.log(JSON.stringify(req.body, null, 2));
+
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
