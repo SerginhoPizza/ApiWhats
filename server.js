c2ca9e9ef8605b29e59a2767c49b7a544ccab3fa
SerginhoPizza <serginhos.monteiro@gmail.com>
2026-02-25 09:20:45 -0300

    Update server.js feat sent menssage

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const axios = require("axios");
+
+const TOKEN = "EAARbTE5PR8MBQ5Tm0bCbmhBGyZCH9J9tiZBczF0zyQYkRucxaAhjL1wLWQlN1WJIZAq1yHe8gxlu8ZBJTZAhTQ9Fim8aZCQfTZBakI8sMPkTj2pjsB8T5Gy6ZBgv7UBtgNvwjTZCZBVlwtPoLCUjhDkzlZBbjhOzjZAR9JuRsmhG5XXYZAIZCQZBwuHFVpUCmVkbSS0EnOXm6I1A2WgAaj7wqQfxPyCDhUocNYorB1fxyq2AVPDk43MTZCPVUoOnJRKzBciiZB820R76FJcXjCS2qWfUQmvcchH9crgZDZD";
+const PHONE_NUMBER_ID = "1066289309890791";
+
 const express = require("express");
 const app = express();
 
@@ -21,20 +26,20 @@ app.get("/webhook", (req, res) => {
 });
 
 // Rota POST (receber mensagens)
-app.post("/webhook", (req, res) => {
+app.post("/webhook", async (req, res) => {
   const body = req.body;
 
-  if (body.object) {
-    const message =
-      body.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
+  const message =
+    body.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
 
-    if (message) {
-      const from = message.from;
-      const text = message.text?.body;
+  if (message) {
+    const from = message.from;
+    const text = message.text?.body;
 
-      console.log("Número:", from);
-      console.log("Mensagem:", text);
-    }
+    console.log("Mensagem recebida:", text);
+
+    // resposta automática simples
+    await enviarMensagem(from, "Olá! 👋 Recebemos sua mensagem.");
   }
 
   res.sendStatus(200);
@@ -45,5 +50,24 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+//Enviar mensagem dentro das 24hrs
+async function enviarMensagem(to, text) {
+  await axios.post(
+    `https://graph.facebook.com/v19.0/${PHONE_NUMBER_ID}/messages`,
+    {
+      messaging_product: "whatsapp",
+      to: to,
+      text: { body: text }
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}
+
 
 
